4aa482433e52322496e9b1bea96a6fb084da9bc2
Sonia Conti <sonia.schembri@gmail.com>
2021-12-10 18:05:43 +0100

    temp move files

---
+++ ContosoFinance-Demo-ARM/AiHandleErrorAttribute.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace WebApplication10.ErrorHandler //namespace will vary based on your project name
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)] 
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {   
+                    var ai = new TelemetryClient();
+                    ai.TrackException(filterContext.Exception);
+                } 
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
